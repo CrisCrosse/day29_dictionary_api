d524b39a907f2ca73486133a170bfe9e459467f9
chris <chrisrossell1999@gmail.com>
2023-09-07 12:05:29 +0100

    Updated webpage and returned word parameter in simple request

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/api/v1/<word>")
+def lookup(word):
+    return {"word": word}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
